f8b3e4c7733a7f82c2678fb07d434eb0902927ad
Danny Kwant <danny.kwant@hva.nl>
2024-06-06 17:02:31 +0200

    Stap 4 - Methode leesNamenBestand geïmplementeerd

--- src/controller/NamenBestandVerwerker.java
+++ src/controller/NamenBestandVerwerker.java
@@ -2,6 +2,8 @@ package controller;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class NamenBestandVerwerker {
@@ -23,7 +25,23 @@ public class NamenBestandVerwerker {
         }
 
         return aantalRegels;
+    }
 
+    public static List<String> leesNamenBestand(String bestandsnaam) {
+        File namenBestand = new File(bestandsnaam);
+        int aantalNamen = telNamenBestand(bestandsnaam);
+        List<String> lijstNamen = new ArrayList<>(aantalNamen);
+
+        try {
+            Scanner invoer = new Scanner(namenBestand);
+            while (invoer.hasNextLine()) {
+                lijstNamen.add(invoer.nextLine());
+            }
+        } catch (FileNotFoundException nietGevonden) {
+            System.out.println("Het bestand is niet gevonden.");
+        }
+
+        return lijstNamen;
     }
 
 } // klasse
--- src/controller/NamenLauncher.java
+++ src/controller/NamenLauncher.java
@@ -1,5 +1,8 @@
 package controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static controller.NamenBestandVerwerker.*;
 
 public class NamenLauncher {
@@ -11,7 +14,34 @@ public class NamenLauncher {
         final String LOCATIE_GESORTEERDE_NAMEN = "resources/GesorteerdeNamenOrigineel.txt";
 
         System.out.printf("In extra namen zitten %d namen. %n", telNamenBestand(LOCATIE_EXTRA_NAMEN));
-        System.out.printf("In gesorteerde namen zitten %d namen. %n", telNamenBestand(LOCATIE_GESORTEERDE_NAMEN));
+        System.out.printf("In gesorteerde namen zitten %d namen. %n%n", telNamenBestand(LOCATIE_GESORTEERDE_NAMEN));
+
+        List<String> mijnExtraLijst = new ArrayList<>(leesNamenBestand(LOCATIE_EXTRA_NAMEN));
+        List<String> mijnGesorteerdeLijst = new ArrayList<>(leesNamenBestand(LOCATIE_GESORTEERDE_NAMEN));
+
+        for (int i = 0; i < mijnExtraLijst.size(); i++) {
+            if (i == 0) {
+                System.out.print("De extra lijst bevat de volgende namen: ");
+            }
+            else if (i != mijnExtraLijst.size() - 1) {
+                System.out.printf("%s, ", mijnExtraLijst.get(i));
+            }
+            else {
+                System.out.printf("%s.%n", mijnExtraLijst.get(i));
+            }
+        }
+
+        for (int i = 0; i < mijnGesorteerdeLijst.size(); i++) {
+            if (i == 0) {
+                System.out.print("De gesorteerde lijst bevat de volgende namen: ");
+            }
+            else if (i != mijnGesorteerdeLijst.size() - 1) {
+                System.out.printf("%s, ", mijnGesorteerdeLijst.get(i));
+            }
+            else {
+                System.out.printf("%s.%n", mijnGesorteerdeLijst.get(i));
+            }
+        }
 
     } // main
 
